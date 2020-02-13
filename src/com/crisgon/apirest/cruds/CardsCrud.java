5bd2289783b4848ca6160bcd00085fb10974ab00
Cristhian <cristhianbay@gmail.com>
2020-02-13 20:53:33 +0100

    Created PlayersCrud class

---
+++ src/com/crisgon/apirest/cruds/CardsCrud.java
@@ -0,0 +1,54 @@
+package com.crisgon.apirest.cruds;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by @cristhian-jg on 13/02/2020
+ *
+ * @author Cristhian González.
+ * 
+ *         Clase que contiene los endpoints para crear, leer, actualizar y
+ *         eliminar cartas de la base de datos.
+ */
+
+@Path("/cardscrud")
+public class CardsCrud {
+
+	/** Endpoint que crea una carta en la base de datos*/
+	@Path("create")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void doCreate() {
+		
+	}
+	
+	/** Endpoint que lee una carta en la base de datos */
+	@Path("read")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public void doRead() {
+		
+	}
+	
+	/** Endpoint que actualiza una carta en la base de datos */
+	@Path("update")
+	@PUT
+	@Produces(MediaType.APPLICATION_JSON)
+	public void doUpdate() {
+		
+	}
+	
+	/** Endpoint que elimina una carta en la base de datos */
+	@Path("delete")
+	@DELETE
+	public void doDelete() {
+		
+	}
+}
