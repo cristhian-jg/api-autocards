5716aad8e267bed8d05f684ad0deb7dc8cde4939
Cristhian <cristhianbay@gmail.com>
2020-02-14 17:51:57 +0100

    Updated CRUD Cartas

--- src/com/crisgon/apirest/cruds/CardsCrud.java
+++ src/com/crisgon/apirest/cruds/CardsCrud.java
@@ -1,5 +1,7 @@
 package com.crisgon.apirest.cruds;
 
+import java.util.ArrayList;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -8,6 +10,10 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.crisgon.apirest.model.Carta;
+import com.crisgon.apirest.services.CartaService;
 
 /**
  * Created by @cristhian-jg on 13/02/2020
@@ -33,8 +39,9 @@ public class CardsCrud {
 	@Path("read")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public void doRead() {
+	public Response doRead() {
 		
+		return null;
 	}
 	
 	/** Endpoint que actualiza una carta en la base de datos */
