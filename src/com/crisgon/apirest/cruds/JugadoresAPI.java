b670289811b4bd497c0a92e5e834b6ddb8ffdeee
Cristhian <cristhianbay@gmail.com>
2020-02-16 20:40:50 +0100

    Implemented necessary endpoints

--- src/com/crisgon/apirest/cruds/JugadoresAPI.java
+++ src/com/crisgon/apirest/cruds/JugadoresAPI.java
@@ -1,5 +1,7 @@
 package com.crisgon.apirest.cruds;
 
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.ArrayList;
 
 import javax.ws.rs.Consumes;
@@ -8,6 +10,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -29,13 +32,27 @@ import com.google.gson.Gson;
 public class JugadoresAPI {
 
 	private static final String TAG = "JugadoresAPI";
-	
+
 	/** Endpoint que crea un jugador en la base de datos */
 	@Path("create")
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	public void doCreate() {
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response doCreate(Jugador jugador) {
 
+		String json;
+
+		JugadorOperations.addJugador(jugador);
+
+		try {
+			jugador = JugadorOperations.getJugador(jugador.getNickname());
+
+			json = new Gson().toJson(jugador);
+
+			return Response.status(Response.Status.OK).entity(json).build();
+		} catch (Exception e) {
+			return Response.status(Response.Status.SEE_OTHER).entity(TAG + ": Creation failed, the user already exists" + e.toString()).build();
+		}
 	}
 
 	/** Endpoint que lee un jugador en la base de datos */
@@ -48,7 +65,7 @@ public class JugadoresAPI {
 		String json;
 
 		try {
-			jugadores = new JugadorOperations().getAll();
+			jugadores = JugadorOperations.getAll();
 
 			json = new Gson().toJson(jugadores);
 
@@ -59,17 +76,48 @@ public class JugadoresAPI {
 	}
 
 	/** Endpoint que actualiza un jugador en la base de datos */
-	@Path("update")
+	@Path("update/{nickname}")
 	@PUT
 	@Produces(MediaType.APPLICATION_JSON)
-	public void doUpdate() {
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response doUpdate(Jugador jugador, @PathParam("nickname") String nickname) {
 
+		
+		String json;
+
+		try {
+			jugador.setNickname(nickname);
+			JugadorOperations.updateJugador(jugador);
+			
+			json = new Gson().toJson(JugadorOperations.getJugador(nickname));
+			return Response.status(Response.Status.OK).entity(json).build();
+
+		} catch (Exception e) {
+			return Response.status(Response.Status.SEE_OTHER).entity(TAG + ": Update failed." + e.toString()).build();
+
+		}
 	}
 
 	/** Endpoint que elimina un jugador en la base de datos */
-	@Path("delete")
+	@Path("delete/{nickname}")
 	@DELETE
-	public void doDelete() {
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response doDelete(@PathParam("nickname") String nickname) {
+
+		Jugador jugador;
+		String json;
+
+		try {
+			json = new Gson().toJson(JugadorOperations.getJugador(nickname));
+			
+			JugadorOperations.deleteJugador(nickname);
+
+			return Response.status(Response.Status.OK).entity(json).build();
+
+		} catch (Exception e) {
+			return Response.status(Response.Status.SEE_OTHER).entity(TAG + ": Deletion failed." + e.toString()).build();
+
+		}
 
 	}
 
