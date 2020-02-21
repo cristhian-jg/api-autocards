fcf5ea8b5ccf8fba1bcc025ee395a4e492b95cef
Cristhian <cristhianbay@gmail.com>
2020-02-21 02:12:44 +0100

    Refactorizada clase juego

--- src/com/crisgon/apirest/cruds/CartasAPI.java
+++ src/com/crisgon/apirest/cruds/CartasAPI.java
@@ -10,6 +10,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -51,6 +52,15 @@ public class CartasAPI {
 	public Response doRead() {
 		ArrayList<Carta> cartas = CartaOperations.getAll();
 		String json = new Gson().toJson(cartas);
+		return Response.status(Response.Status.OK).entity(json).build();
+	}
+	
+	@Path("readUna")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response doReadUna(@QueryParam("id") String id) {
+		Carta carta = CartaOperations.getCarta(id);
+		String json = new Gson().toJson(carta);
 		return Response.status(Response.Status.OK).entity(json).build();
 	}
 
