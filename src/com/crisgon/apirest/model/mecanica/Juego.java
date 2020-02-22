2810b3d5e94d53ebcb2985355ba896f5539d24b8
Cristhian <cristhianbay@gmail.com>
2020-02-22 22:22:57 +0100

    Se ha añadido el numero de mano al juego

--- src/com/crisgon/apirest/model/mecanica/Juego.java
+++ src/com/crisgon/apirest/model/mecanica/Juego.java
@@ -9,51 +9,52 @@ public class Juego {
 	private int idGame;
 	private ArrayList<Carta> cartasJugador;
 	private ArrayList<Carta> cartasMaquina;
-	private String primerTurno;
+	private int numMano;
 	
-	public Juego() {
-	}
 	
-	public Juego(int idGame, ArrayList<Carta> cartasJugador, ArrayList<Carta> cartasMaquina, String idSession) {
+	
+	public Juego(int idGame, ArrayList<Carta> cartasJugador, ArrayList<Carta> cartasMaquina, int numMano) {
 		this.idGame = idGame;
 		this.cartasJugador = cartasJugador;
 		this.cartasMaquina = cartasMaquina;
-		this.primerTurno = idSession;
+		this.numMano = numMano;
 	}
 	
-	public Juego(int idGame, ArrayList<Carta> cartasJugador, ArrayList<Carta> cartasMaquina) {
-		super();
-		this.idGame = idGame;
-		this.cartasJugador = cartasJugador;
-		this.cartasMaquina = cartasMaquina;
-		this.primerTurno = "CPU";
-	}
-
 	public int getIdGame() {
 		return idGame;
 	}
-
 	public void setIdGame(int idGame) {
 		this.idGame = idGame;
 	}
-
+	
 	public ArrayList<Carta> getCartasJugador() {
 		return cartasJugador;
 	}
+	
 	public void setCartasJugador(ArrayList<Carta> cartasJugador) {
 		this.cartasJugador = cartasJugador;
 	}
+	
 	public ArrayList<Carta> getCartasMaquina() {
 		return cartasMaquina;
 	}
+	
 	public void setCartasMaquina(ArrayList<Carta> cartasMaquina) {
 		this.cartasMaquina = cartasMaquina;
 	}
-	public String getPrimerTurno() {
-		return primerTurno;
+	
+	public int getNumMano() {
+		return numMano;
 	}
-	public void setPrimerTurno(String primerTurno) {
-		this.primerTurno = primerTurno;
+	
+	public void setNumMano(int numMano) {
+		this.numMano = numMano;
+	}
+
+	@Override
+	public String toString() {
+		return "Juego [idGame=" + idGame + ", cartasJugador=" + cartasJugador + ", cartasMaquina=" + cartasMaquina
+				+ ", numMano=" + numMano + "]";
 	}
 	
 }
