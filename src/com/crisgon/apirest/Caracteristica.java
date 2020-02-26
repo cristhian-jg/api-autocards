e5f91a0c7c6a1516560b0b595e9669405343efb1
Cristhian <cristhianbay@gmail.com>
2020-02-26 13:41:22 +0100

    Se ha dejado de utilizar este enumerado

--- src/com/crisgon/apirest/Caracteristica.java
+++ src/com/crisgon/apirest/Caracteristica.java
@@ -1,5 +1,11 @@
 package com.crisgon.apirest;
 
-public enum Caracteristica {
+import java.io.Serializable;
+
+/**
+ * Clase no utilizada, tuve dificultades para usar ENUM atraves de Retrofit por
+ * lo que ahora esta repesentado en String.
+ */
+public enum Caracteristica implements Serializable {
 	MOTOR, POTENCIA, VELOCIDAD, CILINDROS, REVOLUCIONES, CONSUMO
 }
